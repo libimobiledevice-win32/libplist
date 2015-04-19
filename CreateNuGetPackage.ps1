35cf8d00ee022c7d7f614fdaf756dfaf4eef6e6a
Frederik Carlier <frederik.carlier@quamotion.mobi>
2015-04-19 18:49:43 +0200

    Try to fix package creation: when a token is replaced, write to a new file and build from there

--- CreateNuGetPackage.ps1
+++ CreateNuGetPackage.ps1
@@ -2,9 +2,11 @@ Param(
   [string]$build
 )
 
+Write-Host Changing build number to $build
+
 # Update the build number
-(gc .\libplist.autoconfig).replace('{build}', $build)|sc .\libplist.autoconfig
+(gc .\libplist.autoconfig).replace('{build}', $build)|sc .\libplist.out.autoconfig
 
 # Create the NuGet package
 Import-Module "C:\Program Files (x86)\Outercurve Foundation\modules\CoApp"
-Write-NuGetPackage .\libplist.autoconfig
+Write-NuGetPackage .\libplist.out.autoconfig
