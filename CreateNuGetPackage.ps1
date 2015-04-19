dc8a32d0d4771d9af0dcb9bc44cd5440872dd806
Frederik Carlier <frederik.carlier@quamotion.mobi>
2015-04-19 18:20:15 +0200

    Include build number in NuGet package number

--- CreateNuGetPackage.ps1
+++ CreateNuGetPackage.ps1
@@ -1,2 +1,10 @@
+Param(
+  [string]$build
+)
+
+# Update the build number
+(gc .\libplist.autoconfig).replace('{build}', $build)|sc .\libplist.autoconfig
+
+# Create the NuGet package
 Import-Module "C:\Program Files (x86)\Outercurve Foundation\modules\CoApp"
 Write-NuGetPackage .\libplist.autoconfig
